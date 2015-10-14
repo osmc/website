55a2135aec3fa543ce458c9cb9a11f0e35aab7f0
Mark Theis Madsen <marktheis@users.noreply.github.com>
2015-10-14 18:41:30 +0200

    wiki: allow curl to follow redirects (if topic link changes)

--- wiki.php
+++ wiki.php
@@ -32,6 +32,7 @@ function get_json_obj($url) {
   $ch = curl_init();
   curl_setopt($ch, CURLOPT_URL, $url);
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
   $output = curl_exec($ch);
   $errno = curl_errno($ch);
   curl_close($ch);
