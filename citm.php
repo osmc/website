d7fdb057455d880300e1a0481fa44f4ada05424c
Sam Nazarko <email@samnazarko.co.uk>
2015-10-19 02:49:37 +0100

    Wordpress: use Singapore for CITM operations
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- citm.php
+++ citm.php
@@ -1,6 +1,6 @@
 <?php
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, "http://46.37.189.135/osmc/download/" . $_GET["citm"]);
+curl_setopt($ch, CURLOPT_URL, "http://singapore.mirror.osmc.tv/osmc/download/" . $_GET["citm"]);
 curl_setopt($ch, CURLOPT_HEADER, 0);
 curl_setopt($ch, CURLOPT_CONNECTTIMEOUT , 20);
 curl_setopt($ch, CURLOPT_TIMEOUT, 20); //timeout in seconds
