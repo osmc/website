696269210469488adffc35a577219f83efd28f3a
Mark Theis Madsen <marktheis@users.noreply.github.com>
2015-11-21 23:15:29 +0100

    scroll to installation section on device click

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -100,6 +100,12 @@ if ($("body").hasClass(homeClass)) {
 };
 
 // Download
+$(".download-devices-wrap").click(function () {
+	$("html, body").animate({
+		"scrollTop": $("#installation").offset().top - 20
+	}, 600);
+});
+
 $(".download-disk-button").click(function () {
 	var tables = $(".download-tables");
 	if (tables.hasClass("show")) {
