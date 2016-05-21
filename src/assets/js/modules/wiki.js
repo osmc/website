2ff0b71472b9108b3b1d0970ff294f2726322a7e
Mark Theis Madsen <marktheism@gmail.com>
2016-05-21 22:58:47 +0200

    no results on wiki search

--- src/assets/js/modules/wiki.js
+++ src/assets/js/modules/wiki.js
@@ -1,27 +1,46 @@
 if ($("body").hasClass("page-wiki")) {
-	$(".wiki-search-input").on("input propertychange", function () {
-		var val = $(this).val();
-		var search = val.toLowerCase();
+    
+  var wikiCheck = function() {
+    $(".wiki-cat").each(function() {
+      var noResults =  $(this).find(".wiki-cat-noresults");
+      var items = $(this).find("ul li").not(".hide, .wiki-cat-noresults");
+      
+      if (items.length === 0) {
+        noResults.addClass("show");
+      } else {
+        noResults.removeClass("show");
+      }
+    });
+  };
+  
+  var wikiSearch = function(input) {
+    var val = input.val();
+    var search = val.toLowerCase();
+    
+    $(".wiki-cat-list li a").each(function () {
+      var title = $(this).attr("data-name");
+      $(this).text(title);
+      var match = title.toLowerCase().search(search);
 
-		$(".wiki-cat-list li a").each(function () {
-			var title = $(this).attr("data-name");
-			$(this).text(title);
-			var match = title.toLowerCase().search(search);
-
-			if (match > -1) {
-				$(this).parent().removeClass("hide");
-
-				if (val.length > 0) {
-					var start = match;
-					var end = match + search.length;
-					var index = title.substring(start, end);
-					var newText = title.replace(index, "<span class='highlight'>" + index + "</span>");
-					$(this).html(newText);
-				}
-			} else {
-				$(this).parent().addClass("hide");
-				$(this).text(title);
-			}
-		});
-	});
+      if (match > -1) {
+        $(this).parent().removeClass("hide");
+        
+        if (val.length > 0) {
+          var start = match;
+          var end = match + search.length;
+          var index = title.substring(start, end);
+          var newText = title.replace(index, "<span class='highlight'>" + index + "</span>");
+          $(this).html(newText);
+        }
+        
+      } else {
+        $(this).parent().addClass("hide");
+      }
+    });
+  };
+  
+  $(".wiki-search-input").on("input propertychange", function () {
+    wikiSearch($(this));
+    wikiCheck();
+  });
 }
