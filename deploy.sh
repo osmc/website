3ea6d1a4432f6913fc055d72c97e8062b452d769
marktheis <marktheis@users.noreply.github.com>
2016-10-02 13:49:20 +0200

    remove unnecessary deploy commands

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,6 @@
 pushd /root/osmc-blog
 service osmcblog stop
 git pull --rebase
-npm install --production
-## Hack hack hack for NPM issue #6170
-while pgrep npm; do sleep 5; done
 service osmcblog start
 # Give time for Ghost to start, then purge cache
 sleep 60
