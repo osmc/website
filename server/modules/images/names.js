504b8b1d0d2a43201b064b43dfd74ce864767f19
Sam Nazarko <email@samnazarko.co.uk>
2017-01-30 01:31:57 +0000

    Update website for Vero 4K
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- server/modules/images/names.js
+++ server/modules/images/names.js
@@ -1,8 +1,9 @@
 var names = {
-  versions_rbp: "Raspberry Pi 1 / Zero",
-  versions_rbp2: "Raspberry Pi 2 / 3",
+  versions_vero3: "Vero 4K",
   versions_vero1: "Vero",
   versions_vero2: "Vero 2",
+  versions_rbp: "Raspberry Pi 1 / Zero",
+  versions_rbp2: "Raspberry Pi 2 / 3",
   versions_appletv: "Apple TV 1"
 };
 
