ee82e09df814a4e7bca15438dd6b4cf94249ca55
Sam Nazarko <email@samnazarko.co.uk>
2017-03-21 18:05:59 +0000

    downloads: make it clearer Zero W is supported
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- server/modules/images/names.js
+++ server/modules/images/names.js
@@ -2,7 +2,7 @@ var names = {
   versions_vero3: "Vero 4K",
   versions_vero1: "Vero",
   versions_vero2: "Vero 2",
-  versions_rbp: "Raspberry Pi 1 / Zero",
+  versions_rbp: "Raspberry Pi 1 / Zero / Zero W",
   versions_rbp2: "Raspberry Pi 2 / 3",
   versions_appletv: "Apple TV 1"
 };
