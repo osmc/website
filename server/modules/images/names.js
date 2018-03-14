9a9412c3bca6fb31516940fdafe0b93858e61870
Sam Nazarko <email@samnazarko.co.uk>
2018-03-14 13:45:53 +0000

    3+ on dl page
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- server/modules/images/names.js
+++ server/modules/images/names.js
@@ -3,7 +3,7 @@ var names = {
   versions_vero1: "Vero",
   versions_vero2: "Vero 2",
   versions_rbp: "Raspberry Pi 1 / Zero / Zero W",
-  versions_rbp2: "Raspberry Pi 2 / 3",
+  versions_rbp2: "Raspberry Pi 2 / 3 / 3+",
   versions_appletv: "Apple TV 1"
 };
 
