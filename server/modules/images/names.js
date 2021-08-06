e42e992dca6a78b111dd8de40702f22f335f089b
Sam Nazarko <email@samnazarko.co.uk>
2021-08-06 05:59:38 +0100

    names.js: add  pi4/400 support; re-order pi versions
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- server/modules/images/names.js
+++ server/modules/images/names.js
@@ -2,8 +2,9 @@ var names = {
   versions_vero3: "Vero 4K / 4K +",
   versions_vero1: "Vero",
   versions_vero2: "Vero 2",
-  versions_rbp: "Raspberry Pi 1 / Zero / Zero W",
+  versions_rbp4: "Raspberry Pi 4 / 400",
   versions_rbp2: "Raspberry Pi 2 / 3 / 3+",
+  versions_rbp: "Raspberry Pi 1 / Zero / Zero W",
   versions_appletv: "Apple TV 1"
 };
 
