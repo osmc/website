1c74beef872cb51b0aa5faee7aa10e4ed2f73238
Sam Nazarko <email@samnazarko.co.uk>
2023-06-28 00:43:38 +0100

    images: add Vero V to download page
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- server/modules/images/names.js
+++ server/modules/images/names.js
@@ -1,6 +1,7 @@
 var names = {
-  versions_vero3: "Vero 4K / 4K +",
+  versions_vero5: "Vero V",
   versions_vero1: "Vero",
+  versions_vero3: "Vero 4K / 4K +",
   versions_vero2: "Vero 2",
   versions_rbp4: "Raspberry Pi 4 / 400",
   versions_rbp2: "Raspberry Pi 2 / 3 / 3+ / Zero W 2",
